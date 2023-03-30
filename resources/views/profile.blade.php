8388c782223e74118202269e05c8e5d0fdeeaaaa
VanessaKwan <vanessakwan17@gmail.com>
2023-03-30 15:17:47 +0700

    Navigation Bar

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,9 +3,37 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Vanessa's Profile</title>
+  <link rel="shortcut icon" href="Assets/Logo.png" type="image/x-icon">
   @vite('resources/css/app.css')
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
+  <div class="flex flex-row">
+    <div id="navbar" class="dark:bg-lime-950 flex flex-row items-center w-full">
+      <a href=""><img class="mx-[0.5vw] h-[6.5vw] w-[6.5vw]"src="Assets/Logo.png" alt=""></a>
+      <p class="text-white">Vanessa's Profile</p>
+      <div class="text-white flex flex-row text-[1.7vw] justify-items-end">
+        <a href="#aboutMe" class="cursor-pointer ml-[43vw] hover:underline underline-offset-4">About Me</a>
+        <a href="#skills" class="cursor-pointer ml-[7vw] hover:underline underline-offset-4">Skills</a>
+        <a href="#reachMe" class="cursor-pointer ml-[7vw] hover:underline underline-offset-4">Reach Me</a>
+      </div>
+    </div>
+
+    <section id="aboutMe">
+      <h1 class="mt-[6.75vw]">test</h1>
+      <p>bro</p>
+    </section>
+
+    <section id="skills">
+      <h1>2</h1>
+    </section>
+
+    <section id="reachMe">
+      <h1>3</h1>
+    </section>
+  </div>
+  
   
 </body>
 </html>
